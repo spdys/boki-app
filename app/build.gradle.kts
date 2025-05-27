a6592b495d79adb51d4c0e0544fdd2eb4ad2f471
ahjadi <alihussainaljadi@gmail.com>
2025-05-27 20:00:18 +0300

    added viewmodel dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("androidx.core:core-ktx:1.12.0")
