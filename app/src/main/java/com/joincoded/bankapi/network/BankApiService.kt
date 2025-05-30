c071cfbcfd527f132da001d1a5fdc1a6fa773019
ahjadi <alihussainaljadi@gmail.com>
2025-05-30 13:19:18 +0300

    retrofit for new transfer controller

--- app/src/main/java/com/joincoded/bankapi/network/BankApiService.kt
+++ app/src/main/java/com/joincoded/bankapi/network/BankApiService.kt
@@ -111,7 +111,15 @@ interface BankApiService {
         @Body request: TransactionHistoryRequest
     ) : Response<List<TransactionHistoryResponse>>
 
-    interface AuthApiService {
+    @POST("transactions/v1/transfer")
+    suspend fun transfer(
+        @Header(Constants.authorization) token: String?,
+        @Body request: TransferRequest
+    ) : Response<TransferResponse>
+
+}
+
+interface AuthApiService {
 
         @POST("api/v1/users/register")
         suspend fun registerUser(@Body creationRequest: UserCreationRequest): Response<UserCreationResponse>
