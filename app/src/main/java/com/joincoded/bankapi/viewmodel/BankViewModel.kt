197713fe5c7ea3f6d5db5b8498dfe2269525e4b6
ahjadi <alihussainaljadi@gmail.com>
2025-05-30 16:07:59 +0300

    view model updated for user registration and kyc submission

--- app/src/main/java/com/joincoded/bankapi/viewmodel/BankViewModel.kt
+++ app/src/main/java/com/joincoded/bankapi/viewmodel/BankViewModel.kt
@@ -5,26 +5,57 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.joincoded.bankapi.data.AmountChange
 import com.joincoded.bankapi.data.AuthenticationRequest
-import com.joincoded.bankapi.data.User
+import com.joincoded.bankapi.data.KYCRequest
+import com.joincoded.bankapi.data.UserCreationRequest
 import com.joincoded.bankapi.data.response.TokenResponse
-import com.joincoded.bankapi.network.BankApiService
 import com.joincoded.bankapi.network.RetrofitHelper
 import kotlinx.coroutines.launch
 
 class BankViewModel : ViewModel() {
-    private val apiService = RetrofitHelper.getAuthInstance()
+    private val apiAuthService = RetrofitHelper.getAuthInstance()
     var token: String? by mutableStateOf(null)
+    private val apiBankService = RetrofitHelper.getBankingInstance()
 
     fun getToken(username: String, password: String) {
         viewModelScope.launch {
             try {
-                val response = apiService.getToken(AuthenticationRequest(username, password))
+                val response = apiAuthService.getToken(AuthenticationRequest(username, password))
                 token = response.body()?.token
             } catch (e: Exception) {
                 println(e)
             }
         }
+    }
+
+    fun register(username: String, password: String){
+        viewModelScope.launch {
+            try {
+                val response = apiAuthService.registerUser(UserCreationRequest(username, password))
+            } catch (e: Exception){
+                println(e)
+            }
+        }
+    }
+
+    fun submitKYC(
+        fullName: String,
+        phone: String,
+        email: String,
+        civilId: String,
+        address: String,
+        dateOfBirth: String,
+    ) {
+        viewModelScope.launch {
+            try {
+                val response = apiBankService.submitKYC(
+                    TokenResponse(token).getBearerToken(),
+                    KYCRequest(fullName, phone, email, civilId, address, dateOfBirth)
+                )
+            } catch (e: Exception){
+                println(e)
+            }
+        }
+
     }
 }
