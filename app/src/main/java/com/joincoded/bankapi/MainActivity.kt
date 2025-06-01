fda9dd966d8b73cad7c2224a830f405f1b2fbbb8
ahjadi <alihussainaljadi@gmail.com>
2025-06-01 11:02:14 +0300

    trying the simple reg screen

--- app/src/main/java/com/joincoded/bankapi/MainActivity.kt
+++ app/src/main/java/com/joincoded/bankapi/MainActivity.kt
@@ -10,30 +10,32 @@ import androidx.compose.ui.Modifier
 import com.joincoded.bankapi.screens.LoginScreen
 import com.joincoded.bankapi.ui.theme.BankAPITheme
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.joincoded.bankapi.testingcomposes.SimpleRegistrationScreen
+import com.joincoded.bankapi.testingcomposes.TokenLoginScreen
 import com.joincoded.bankapi.viewmodel.BankViewModel
 
 
 class MainActivity : ComponentActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val bankViewModel: BankViewModel = viewModel()
             BankAPITheme  {
-              
-                // A surface container using the 'background' color from the theme
 
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
 
-                    LoginScreen(
-                        bankViewModel = bankViewModel,
-                        onLoginSuccess = {},
-                        navigateToPinLogin = {},
-                    )
+
+                    SimpleRegistrationScreen(bankViewModel)
+
+//                    LoginScreen(
+//                        bankViewModel = bankViewModel,
+//                        onLoginSuccess = {},
+//                        navigateToPinLogin = {},
+//                    )
 
                 }
             }
