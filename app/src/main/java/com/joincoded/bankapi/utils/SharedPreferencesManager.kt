433df21a3b4fe5142862417435a6a2cb7ffe224f
Sarah <sarahalenezi96@outlook.com>
2025-06-02 03:21:11 +0300

    edited the shared preferences to store full name instead of just first name just to have more flexibility - splitting the first name is handled in screens files as needed

--- app/src/main/java/com/joincoded/bankapi/utils/SharedPreferencesManager.kt
+++ app/src/main/java/com/joincoded/bankapi/utils/SharedPreferencesManager.kt
@@ -6,18 +6,18 @@ import androidx.core.content.edit
 
 object SharedPreferencesManager {
     private const val PREF_NAME = "BokiPrefs"
-    private const val KEY_FIRST_NAME = "first_name"
+    private const val KEY_FULL_NAME = "full_name"
 
     private fun getPrefs(context: Context): SharedPreferences {
         return context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
     }
 
     fun saveUserName(context: Context, name: String) {
-        getPrefs(context).edit { putString(KEY_FIRST_NAME, name) }
+        getPrefs(context).edit { putString(KEY_FULL_NAME, name) }
     }
 
     fun getSavedUserName(context: Context): String {
-        return getPrefs(context).getString(KEY_FIRST_NAME, "") ?: ""
+        return getPrefs(context).getString(KEY_FULL_NAME, "") ?: ""
     }
 
     fun clearAll(context: Context) {
